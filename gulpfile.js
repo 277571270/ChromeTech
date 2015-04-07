2ae73e53bf42dc0aebc30cb03d1adffb724e814a
TinyBear <xiongbin1989427@gmail.com>
2015-04-07 23:58:46 +0800

    新增 gulp插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 var paths = {
     scripts: 'js/**/*.js'
@@ -10,3 +13,5 @@ gulp.task('lint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+//gulp.task('default', ['watch', 'scripts', 'images']);
